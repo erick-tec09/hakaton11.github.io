888fc5663339533d82990ce61beb9b9dd46295c4
erick antony <erick.tec09@gmail.com>
2020-10-17 12:55:33 -0500

    Segundo reto terminado

---
+++ 2-Reto/js.js
@@ -0,0 +1,24 @@
+//variables
+let addbutton = document.getElementById("addbutton");
+let inputfield = document.getElementById("inputfield");
+let list = document.getElementById("list");
+
+
+//functions
+addbutton.addEventListener("click", function () {
+  let field = document.getElementById("inputfield").value;
+  let task = document.createElement("li");
+  if (field ==='') {
+    
+     alert('El formulario esta vacio')
+    ;
+  } else {
+    task.classList.add("task");
+    task.innerHTML = inputfield.value;
+    inputfield.value = "";
+    list.appendChild(task);
+    task.addEventListener("dblclick", function () {
+      list.removeChild(task);
+    });
+  }
+});
